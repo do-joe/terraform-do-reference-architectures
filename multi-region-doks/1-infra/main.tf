f0961ed3b840cb4d87c7d02bf47770c28dc8f479
Joe Keegan <jkeegan@digitalocean.com>
2025-04-03 15:51:23 -0400

    doks deploy

---
+++ multi-region-doks/1-infra/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.50"
+    }
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = "~> 2.0"
+#     }
+#     helm = {
+#       source  = "hashicorp/helm"
+#       version = "3.0.0-pre2"
+#     }
+#     http = {
+#       source  = "hashicorp/http"
+#       version = "~> 3.0"
+#     }
+  }
+
+}
+
+module "multi_region_vpc" {
+  source             = "../../../terraform-do-multi-region-vpc"
+  name_prefix        = var.name_prefix
+  primary_region     = var.primary_region
+  primary_ip_range   = var.primary_vpc_ip_range
+  secondary_region   = var.secondary_region
+  secondary_ip_range = var.secondary_vpc_ip_range
+}
+
+resource "digitalocean_kubernetes_cluster" "primary_cluster" {
+  name                             = "${var.name_prefix}-${var.primary_region}"
+  tags                             = [var.name_prefix]
+  region                           = var.primary_region
+  version                          = var.doks_version
+  cluster_subnet                   = var.primary_pod_ip_range
+  service_subnet                   = var.primary_service_ip_range
+  vpc_uuid                         = module.multi_region_vpc.primary_vpc_id
+  auto_upgrade                     = var.doks_auto_upgrade
+  destroy_all_associated_resources = var.doks_destroy_all_associated_resources
+  ha                               = var.doks_ha
+  registry_integration             = var.doks_registry_integration
+  routing_agent {
+    enabled = var.doks_routing_agent
+  }
+  node_pool {
+    name       = "${var.name_prefix}-${var.primary_region}-${var.node_pool_name}"
+    size       = var.node_pool_size
+    auto_scale = true
+    min_nodes  = var.node_pool_min
+    max_nodes  = var.node_pool_max
+    tags       = [var.name_prefix]
+  }
+}
+
+resource "digitalocean_kubernetes_cluster" "secondary_cluster" {
+  name                             = "${var.name_prefix}-${var.secondary_region}"
+  tags                             = [var.name_prefix]
+  region                           = var.secondary_region
+  version                          = var.doks_version
+  cluster_subnet                   = var.secondary_pod_ip_range
+  service_subnet                   = var.secondary_service_ip_range
+  vpc_uuid                         = module.multi_region_vpc.secondary_vpc_id
+  auto_upgrade                     = var.doks_auto_upgrade
+  destroy_all_associated_resources = var.doks_destroy_all_associated_resources
+  ha                               = var.doks_ha
+  registry_integration             = var.doks_registry_integration
+  routing_agent {
+    enabled = var.doks_routing_agent
+  }
+  node_pool {
+    name       = "${var.name_prefix}-${var.secondary_region}-${var.node_pool_name}"
+    size       = var.node_pool_size
+    auto_scale = true
+    min_nodes  = var.node_pool_min
+    max_nodes  = var.node_pool_max
+    tags       = [var.name_prefix]
+  }
+}
+
---
+++ multi-region-doks/1-infra/variables.tf
@@ -0,0 +1,103 @@
+variable "name_prefix" {
+  description = "prefix used for the resources created in this module"
+  type        = string
+}
+
+variable "primary_region" {
+  description = "DO region slug for the primary region"
+  type        = string
+}
+
+variable "primary_vpc_ip_range" {
+  description = "CIDR notation for subnet used for primary region VPC"
+  type        = string
+  nullable    = true
+  default     = null
+}
+
+variable "secondary_region" {
+  description = "DO region slug for the secondary region"
+  type        = string
+}
+
+variable "primary_pod_ip_range" {
+  description = "CIDR notation for the subnet used for pod IPs in the primary region"
+}
+
+variable "primary_service_ip_range" {
+  description = "CIDR notation for the subnet used for service IPs in the primary region"
+}
+
+
+variable "secondary_vpc_ip_range" {
+  description = "CIDR notation for subnet used for secondary region VPC"
+  type        = string
+  nullable    = true
+  default     = null
+}
+
+variable "secondary_pod_ip_range" {
+  description = "CIDR notation for the subnet used for pod IPs in the secondary region"
+}
+
+variable "secondary_service_ip_range" {
+  description = "CIDR notation for the subnet used for service IPs in the secondary region"
+}
+
+
+variable "doks_version" {
+  description = "Version to use for DOKS clusters"
+  type        = string
+}
+
+variable "doks_auto_upgrade" {
+  description = "If auto upgrade is configured for DOKS clusters"
+  type        = bool
+  default     = false
+}
+
+variable "doks_ha" {
+  description = "If DOKS clusters deploy with HA control plane"
+  type        = bool
+  default     = true
+}
+
+variable "doks_destroy_all_associated_resources" {
+  description = "If all resources created via the Kubernetes API (load balancers, volumes, and volume snapshots) will be destroyed"
+  type        = bool
+  default     = false
+}
+
+variable "doks_registry_integration" {
+  description = "If DOKS clusters deployed with DOCR integration. Requires DOCR to be enabled within the team"
+  type        = bool
+  default     = false
+}
+
+variable "doks_routing_agent" {
+  description = "If DOKS clusters deployed with routing agent enabled"
+  type        = bool
+  default     = false
+}
+
+variable "node_pool_name" {
+  description = "Name of the first node pool created for the DOKS cluster"
+  type        = string
+  default     = "default"
+}
+
+variable "node_pool_size" {
+  description = "slug identifier for the type of Droplet"
+  type        = string
+}
+
+variable "node_pool_min" {
+  description = "Min number of nodes to include in the node pool"
+  type        = number
+}
+
+variable "node_pool_max" {
+  description = "Max number of nodes to include in the node pool"
+  type        = number
+}
+
